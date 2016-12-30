115f9094b351fdbb3ef6b208bb920ded38f8f39c
Jai Broome <jbroome@uw.edu>
2016-12-30 11:33:20 -0800

    vectorized dbGetTraitInfoByAccession

--- R/dbGetTraitInfoByAccession.R
+++ R/dbGetTraitInfoByAccession.R
@@ -1,10 +1,17 @@
 #' Get trait info by dbGaP phv number
 #' @param db A database connection
-#' @param dbgap_trait_accession The dbGaP variable accession, as an integer
+#' @param dbgap_trait_accessions A vector of dbGaP variable accessions, as integers
 #' @export
-dbGetTraitInfoByAccession <- function(db, dbgap_trait_accession){
+dbGetTraitInfoByAccession <- function(db, dbgap_trait_accessions){
+  accs_string <- paste("dbgap_trait_accession =",
+                      dbgap_trait_accessions,
+                      "OR",
+                      collapse = " ")
+  # Remove trailing ' OR'
+  accs_string <- gsub('.{3}$', '', accs_string)
+
   query <- paste("SELECT dataset_accession, source_trait_id, trait_name, trait_description",
-                 "FROM view_source_trait",
-                 sprintf("WHERE dbgap_trait_accession = %s", dbgap_trait_accession))
+                 "FROM view_source_trait WHERE",
+                 accs_string)
   dbGetQuery(db, query)
 }
