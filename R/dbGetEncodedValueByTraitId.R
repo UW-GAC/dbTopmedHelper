bbaeb7f8a1e8821f7f1f14355b1a8dfd99598e72
Jai Broome <jbroome@uw.edu>
2017-03-09 11:21:49 -0800

    Simplify dbGetEncodedValue query string

--- R/dbGetEncodedValueByTraitId.R
+++ R/dbGetEncodedValueByTraitId.R
@@ -4,14 +4,10 @@
 #' @import dbTopmed
 #' @export
 dbGetEncodedValueByTraitId <- function(db, trait_ids){
-  ids_string <- paste("e.source_trait_id =", trait_ids, "OR", collapse = " ")
-  # Remove trailing ' OR'
-  ids_string <- gsub('.{3}$', '', ids_string)
-
-  query <- paste("SELECT t.source_trait_id, t.dbgap_variable_accession, t.trait_name, ",
+  query <- paste("SELECT t.source_trait_id, t.dbgap_variable_accession, t.trait_name,",
                  "e.category, e.value",
                  "FROM source_trait_encoded_values e",
-                 "LEFT JOIN source_trait t ON e.source_trait_id = t.source_trait_id WHERE ",
-                 ids_string)
+                 "LEFT JOIN source_trait t ON e.source_trait_id = t.source_trait_id WHERE",
+                 sprintf("e.source_trait_id IN (%s)", paste(trait_ids, collapse = ", ")))
   dbGetQuery(db, query)
 }
