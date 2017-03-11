972bea116651017190bbdf29825de034f0aa33e8
Jai Broome <jbroome@uw.edu>
2017-03-10 17:02:37 -0800

    Simplify query for dbGetTraitInfoByAccession, accept accs as strings
    Modify query in dbGetTraitInfoByAccession to use an '... IN (>...)' clause instead of a series of '= ... OR = ...' for each accession. Also modify to accept accessions of both formats: phv012345 or 12345

--- R/dbGetTraitInfoByAccession.R
+++ R/dbGetTraitInfoByAccession.R
@@ -1,18 +1,14 @@
 #' Get trait info by dbGaP phv number
 #' @param db A database connection
-#' @param dbgap_trait_accessions A vector of dbGaP variable accessions, as integers
+#' @param dbgap_trait_accessions A vector of dbGaP variable accessions, as integers or strings with a leading "phv"
 #' @import dbTopmed
 #' @export
-dbGetTraitInfoByAccession <- function(db, dbgap_trait_accessions){
-  accs_string <- paste("dbgap_trait_accession =",
-                      dbgap_trait_accessions,
-                      "OR",
-                      collapse = " ")
-  # Remove trailing ' OR'
-  accs_string <- gsub('.{3}$', '', accs_string)
 
-  query <- paste("SELECT *",
-                 "FROM view_source_trait WHERE",
-                 accs_string)
-  dbGetQuery(db, query)
+dbGetTraitInfoByAccession <- function(db, dbgap_trait_accessions){
+    accs_standardized <- sapply(dbgap_trait_accessions, sub, pattern = "^phv", replacement = "")
+    query <- paste("SELECT *",
+                   "FROM view_source_trait WHERE",
+                   sprintf("dbgap_trait_accession IN (%s)",
+                           paste(accs_standardized, collapse = ", ")))
+    dbGetQuery(db, query)
 }
