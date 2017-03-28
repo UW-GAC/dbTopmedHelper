cdcdc0d7f1b77f92d3953fc43d62e56051088b4a
Jai Broome <jbroome@uw.edu>
2017-03-28 14:27:27 -0700

    Add dbGetTraitInfoByVarName

---
+++ R/dbGetTraitInfoByVarName.R
@@ -0,0 +1,15 @@
+#' Get trait info from a table by pht number and trait names
+#' @param pht A dbGaP accession for a table, as an integer
+#' @param var_names A vector of trait names
+#' @param db A database connection
+#' @import dbTopmed
+#' @export
+dbGetTraitInfoByVarName <- function(pht, var_names, db){
+    query <- paste("SELECT *",
+                   "FROM view_source_trait",
+           sprintf("WHERE dataset_accession = %s", pht),
+           sprintf("AND trait_name IN ('%s');",
+                   paste(var_names, collapse = "', '")))
+    results <- dbGetQuery(db, query)
+    return(results)
+}
